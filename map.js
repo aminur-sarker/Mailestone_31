e69a6b8e6724a000c55c6bae4c08628f95910d5a
aminur-sarker <aminursarker2002@gmail.com>
2023-08-23 23:21:41 +0600

    map

---
+++ map.js
@@ -0,0 +1,14 @@
+// map: which return array
+
+// make double of every element of an array
+const number = [1,2,3,4,5,6,7,8,9,10];
+console.log(number.map(num=>num*2));
+
+
+
+
+
+const friendsName =['Aminur' , 'Arafat' , 'Prosenjit'];
+console.log(friendsName);  // find all the element of an array
+console.log(friendsName.map(name=>name.length));  // find the length of every element
+console.log(friendsName.map(name=>name[0]))  // find the first letter of every element of an array
