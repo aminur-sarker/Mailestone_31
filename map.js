e6d6f81a895455341f94fa933328dc5ebb0eaeb5
aminur-sarker <aminursarker2002@gmail.com>
2023-08-23 23:51:49 +0600

    map, forEach, filter, find

--- map.js
+++ map.js
@@ -4,11 +4,28 @@
 const number = [1,2,3,4,5,6,7,8,9,10];
 console.log(number.map(num=>num*2));
 
-
-
-
-
 const friendsName =['Aminur' , 'Arafat' , 'Prosenjit'];
 console.log(friendsName);  // find all the element of an array
 console.log(friendsName.map(name=>name.length));  // find the length of every element
 console.log(friendsName.map(name=>name[0]))  // find the first letter of every element of an array
+
+
+// forEach: prottek Element niye kaj korbe but return korbe na
+
+
+
+// filter: jara conditon manbe tader array hisebe prottek take return korbe
+
+// find the value which is grater than 6
+const player = [1,2,3,4,5,6,7,8,9,10];
+console.log(player.filter(p=> p > 6));
+
+
+// find the name which length is less than 7
+const friendName =['Aminur' , 'Arafat' , 'Prosenjit'];
+console.log(friendsName.filter(name => name.length < 7));  
+
+
+// find: jara condition puron korbe tader moddhe 1st element k nibe
+const arr = [1,2,3,4,5,6,7,8,9,10];
+console.log(arr.find(a => a > 5))
